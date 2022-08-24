46174410478951e17edcf87e1c65ce0bef12b208
CHAT AIAM <panachat1183@gmail.com>
2022-08-24 12:34:17 +0700

    test

---
+++ Caesar_Rot13/Caesar_Rot13.py
@@ -0,0 +1,25 @@
+chars = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q',
+        'r','s','t','u','v','w','x','y','z']
+input_text = int(input("Enter your number : "))
+text = []
+
+for c in range(input_text):
+    text.append(input("Enter : your text : "))
+n = input("Enter : ")
+
+for x in text:
+    print(x)
+    for i in range (len(chars)):
+        if x == chars[i]: #TODO ถ้าไม่ใส่ชื่อตัวแปรของ list ข้างหน้ามันจะเป็น เลข ถ้าใส่มันจะเป็นค่าใน list
+            if n == 'g':
+                ceasar = (i + 3)%26
+                print("เข้ารหัส Ceasar",chars[ceasar])
+                rot = (i + 13)%26
+                print("เข้ารหัส Rot",chars[rot])
+            elif n == 'f':
+                ceasar = (i - 3)%26
+                print(chars[ceasar],end='')
+                rot = (i - 13)%26
+                print(chars[rot],end='')
+
+        
